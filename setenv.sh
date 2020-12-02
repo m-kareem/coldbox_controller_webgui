bedac50fd870de1a9620910f1cff22af2c73de2b
mkareem <mohammad.kareem@cern.ch>
2020-12-02 01:48:48 -0500

    updating setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,3 @@
 export PYTHONPATH=$PYTHONPATH:$(cd ..; pwd)/coldjiglib2
+export PYTHONPATH=$PYTHONPATH:$(cd ..; pwd)/coldjiglib2/modules
 export PYTHONPATH=$PYTHONPATH:$(cd ..; pwd)/coldbox_controller_webgui/modules
